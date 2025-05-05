987eee557ca6035efbd454846e2236916aaafcd5
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-05 16:04:33 +0530

    latest

--- src/main/java/com/ioc/IocDemoApplication.java
+++ src/main/java/com/ioc/IocDemoApplication.java
@@ -2,12 +2,22 @@ package com.ioc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.ioc.quickones.Task;
 
 @SpringBootApplication
 public class IocDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IocDemoApplication.class, args);
+		ApplicationContext ac=
+				SpringApplication.run(IocDemoApplication.class, args);
+		
+	 Task t=	(Task)ac.getBean("paint");
+	 t.doit();
+	 
+	 t=(Task)ac.getBean("clean");
+	 t.doit();
 	}
 
 }
