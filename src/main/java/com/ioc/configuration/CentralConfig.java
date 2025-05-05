05d5983ff74c35770101d86340e5307654460e66
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-05 15:57:01 +0530

    latest

--- src/main/java/com/ioc/configuration/CentralConfig.java
+++ src/main/java/com/ioc/configuration/CentralConfig.java
@@ -1,11 +1,26 @@
 package com.ioc.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.ioc.quickones.Cleaning;
+import com.ioc.quickones.Painting;
+import com.ioc.quickones.Task;
 
 @Configuration
 public class CentralConfig {
 //local block
 	{
 		System.out.println("Central Config is instantiated");
+	}
+	
+	@Bean("paint")
+	public Task getPainting() {
+		return new Painting();
+	}
+	
+	@Bean("clean")
+	public Task getCleaning() {
+		return new Cleaning();
 	}
 }
